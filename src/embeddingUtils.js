eb26b91ab13d73718f5b1e9f508f6b96db5c84a3
Angel Popov <geleto@gmail.com>
2025-11-25 21:12:23 +0000

    Get rid of the embeddings utils file

--- src/embeddingUtils.js
+++
@@ -1,7 +0,0 @@
-// Stubs for embeddingUtils
-// Stubs for embeddingUtils
-// This file is stripped of external dependencies
-
-export async function createEmbedding(text) {
-    throw new Error('createEmbedding stub - will be replaced with callback');
-}
